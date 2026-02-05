91a30badf679cf871ab0a44aee4b5cf80a0670b3
skiess18 <skiess18@gmail.com>
2026-02-05 19:08:13 +0200

    Update Coffee_machine.py
    Function for money received
    
    We check whether the amount is greater than or equal to the price.

--- Coffee_machine.py
+++ Coffee_machine.py
@@ -46,3 +46,14 @@ def process_coins():
     total += int(input("How many nickles?: ")) * 0.05
     total += int(input("How many pennies?: ")) * 0.01
     return total
+
+def is_transaction_successful(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change}.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print(f"Sorry, that's not enough money. Money refunded")
+        return False
